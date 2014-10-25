def494945d4ead43f10ac506122f1ddd031e8a94
Geoffrey Liu <liug.2012@gmail.com>
2014-10-25 14:43:40 -0700

    Use requests library instead

--- weather.py
+++ weather.py
@@ -1,7 +1,4 @@
-try:
-    import urllib.request as b
-except:
-    import urllib2 as b
+import requests as b
 from geopy.geocoders import Nominatim
 import json
 
@@ -14,5 +11,7 @@ loc = geolocator.geocode(userInput)
 queryString = "https://api.forecast.io/forecast/%s/%s,%s" %(API_KEY, loc.latitude, loc.longitude)
 print(queryString)
 
-rawData = b.urlopen(queryString)
-print(rawData.readline())
+rawData = b.get(queryString)
+jsonData = rawData.json()
+
+print(jsonData)
