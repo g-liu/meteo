2af168964a34f3b06ef6dfaaaee4d4e9b34ff18e
Geoffrey Liu <liug.2012@gmail.com>
2014-10-25 14:11:27 -0700

    read API Key

--- weather.py
+++ weather.py
@@ -1,6 +1,9 @@
 from geopy.geocoders import Nominatim 
 
 geolocator = Nominatim()
+API_KEY = open('API_KEY').readline() # Place your Forecast.IO API Key here
+
 userInput = input("Location? ")
 location = geolocator.geocode(userInput)
+
 print(location.latitude, location.longitude)
