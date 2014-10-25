8d5621842a7e531d282eb0736e26f573a08443ef
Geoffrey Liu <liug.2012@gmail.com>
2014-10-25 14:27:37 -0700

    Now reading data from URL

--- weather.py
+++ weather.py
@@ -1,9 +1,18 @@
-from geopy.geocoders import Nominatim 
+try:
+    import urllib.request as b
+except:
+    import urllib2 as b
+from geopy.geocoders import Nominatim
+import json
 
 geolocator = Nominatim()
 API_KEY = open('API_KEY').readline() # Place your Forecast.IO API Key here
 
 userInput = input("Location? ")
-location = geolocator.geocode(userInput)
+loc = geolocator.geocode(userInput)
 
-print(location.latitude, location.longitude)
+queryString = "https://api.forecast.io/forecast/%s/%s,%s" %(API_KEY, loc.latitude, loc.longitude)
+print(queryString)
+
+rawData = b.urlopen(queryString)
+print(rawData.readline())
